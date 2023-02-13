c3b4f675386a863933b47b19625c3e1a42d98aa8
Leonardo Motta <leonardomotta@ucl.br>
2023-02-13 18:38:12 -0300

    add Seller Service

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using SalesWebMVC.Services;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _sellerService.findAll();
+            return View(list);
+        }
+    }
+}
--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -15,7 +15,7 @@ namespace SalesWebMVC.Data
         }
 
         public DbSet<Department> Department { get; set; }
-        public DbSet<SalesRecord> Seller { get; set; }
+        public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecord { get; set; }
 
     }
